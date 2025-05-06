406772a805651a6bebc09245a86209d7f4c555a6
Dapinder Kaur <172073817+Dapinder-Kaur@users.noreply.github.com>
2025-05-06 14:50:27 -0500

    trying gemini here

--- main.py
+++ main.py
@@ -1,28 +1,19 @@
-import openai
 import requests
-from openai import OpenAI
+import google.generativeai as genai
 import time
 
-client = OpenAI(
-    api_key="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"
-)
+GOOGLE_API_KEY = "AIzaSyBvUiuvXnWsvV3qpBKBPHas9jrBN_YB1jk"
+genai.configure(api_key=GOOGLE_API_KEY)
 
 
 # a chatbot function
-def chatbot_func(input: str, model="gpt-3.5-turbo"):
+def chatbot_func(input: str, model=genai.GenerativeModel("gemini")):
 
     # Make the API request
-    response = client.chat.completions.create(
-        model=model,
-        messages=[{"role": "user", "content": input}],
-    )
+    chat = model.start_chat(history=[])
 
-    # Check if the request was successful
-    if response.status_code == 200:
-        response_data = response.json()
-        return response_data["choices"][0]["message"]["content"]
-    else:
-        return f"Error: {response.status_code}, {response.text}"
+    response = chat.send_message(input, stream=False)
+    return response.text
 
 
 input = input("User: ")
