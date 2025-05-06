676539fb1111eae87df6ac6da6df92943cb6ed84
Dapinder Kaur <172073817+Dapinder-Kaur@users.noreply.github.com>
2025-05-06 14:00:07 -0500

    working code for using open ai's api key

--- main.py
+++ main.py
@@ -1,30 +1,21 @@
 import openai
 import requests
+from openai import OpenAI
 import time
 
-client = openai.OpenAI(
+client = OpenAI(
     api_key="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"
 )
 
 
 # a chatbot function
 def chatbot_func(input: str, model="gpt-3.5-turbo"):
-    time.sleep(5)
-    # Define the API endpoint and headers
-    url = "https://api.openai.com/v1/chat/completions"
-    headers = {
-        "Content-Type": "application/json",
-        "Authorization": f"Bearer {client.api_key}",
-    }
-
-    # Define the data payload
-    data = {
-        "model": model,
-        "messages": [{"role": "user", "content": input}],
-    }
 
     # Make the API request
-    response = requests.post(url, headers=headers, json=data)
+    response = client.chat.completions.create(
+        model=model,
+        messages=[{"role": "user", "content": input}],
+    )
 
     # Check if the request was successful
     if response.status_code == 200:
