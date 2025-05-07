da5d69444d010663ddb5963c99d63bfd960f4add
Dapinder Kaur <172073817+Dapinder-Kaur@users.noreply.github.com>
2025-05-07 08:57:56 -0500

    everything achieved till day 2

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import requests
 import google.generativeai as genai
 import time
 
-GOOGLE_API_KEY = "AIzaSyBvUiuvXnWsvV3qpBKBPHas9jrBN_YB1jk"
+
+GOOGLE_API_KEY = "AIzaSyD_nRcAau-qH_I_1tRQ5RTIaaGR67oBwyY"
 genai.configure(api_key=GOOGLE_API_KEY)
 
 
@@ -12,7 +13,7 @@ def chatbot_func(input: str, model=genai.GenerativeModel("gemini-1.5-pro-002")):
     # Make the API request
     chat = model.start_chat(history=[])
 
-    response = chat.send_message(input, stream=False)
+    response = chat.send_message(input, stream=True)
     return response.text
 
 
