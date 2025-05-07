bebf55bb1092a2119ea4e18b93acc6a4d59c5ff8
Dapinder Kaur <172073817+Dapinder-Kaur@users.noreply.github.com>
2025-05-07 09:32:54 -0500

    fully working model with gemini, step_1 accomplished

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 from google import genai
+import time
 
 client = genai.Client(api_key="AIzaSyCRL3Ujbp6j-Gh1d1kO3PNWBLJqc5a1QGU")
 
 
 def example_usage():
-    response = client.models.generate_content(
-        model="gemini-2.0-flash",
-        contents="Explain how AI works in a few words",
+    response = client.models.generate_content_stream(
+        model="gemini-2.0-flash", contents="Hi, How are you?"
     )
-    return response.text
+    return response
 
 
 def chatbot_actual(input_text):
@@ -19,8 +19,15 @@ def chatbot_actual(input_text):
     return response.text
 
 
-# input_text = input("User: ")
-# response = chatbot_actual(input_text)
-response = example_usage()
+input_text = input("User: ")
+response = chatbot_actual(input_text)
+# response = example_usage()
 
-print(response)
+
+def print_response(response):
+    for chunk in response.split(" "):
+        print(chunk, end=" ")
+        time.sleep(0.1)
+
+
+print_response(response)
