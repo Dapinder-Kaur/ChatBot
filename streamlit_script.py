0b75992539c3b7d7ba990611d93b40676f218409
Dapinder Kaur <172073817+Dapinder-Kaur@users.noreply.github.com>
2025-05-07 15:01:25 -0500

    getting towards the actual outputs using streamlit

--- streamlit_script.py
+++ streamlit_script.py
@@ -1,6 +1,9 @@
 # importing necessary libraries
 import streamlit as st
+import random
+import time
 from PIL import Image
+from google import genai
 import main
 
 
@@ -19,5 +22,43 @@ def streamlit_app():
     left_column.text_input("User: ", placeholder="Type your message here...")
 
 
+def streamlit_app_design_2():
+
+    st.title("Your personal Gemini")
+
+    if "gemini_model" not in st.session_state:
+        st.session_state["gemini_model"] = "gemini-2.0-flash"
+
+    # having the history of the model
+    if "messages" not in st.session_state:
+        st.session_state.messages = []
+
+    # displaying chat messages from history
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
+
+    # Accept user input
+    if prompt := st.chat_input("What is up?"):
+        # display user message in chat message container
+        with st.chat_message("user"):
+            st.markdown(prompt)
+        # Adding message to the chat history
+        st.session_state.messages.append({"role": "user", "content": prompt})
+
+    functions = main.main()
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    # st.session_state.messages.append({"role": "assistant", "content": response})
+
+    # with st.chat_message("assistant"):
+    #     stream = client.models.generate_content_stream(
+    #         model=st.session_state["gemini_model"],
+    #         contents=prompt,
+    #     )
+    #     response = st.write_stream(stream)
+
+    # st.session_state.messages.append({"role": "assistant", "content": response})
+
+
 if __name__ == "__main__":
-    streamlit_app()
+    streamlit_app_design_2()
