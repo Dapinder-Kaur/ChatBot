6d521821cdda48fef08eb6c2b3350f104b500b5e
Dapinder Kaur <172073817+Dapinder-Kaur@users.noreply.github.com>
2025-05-07 11:10:30 -0500

    streamlit with working .py files including main.py

--- streamlit_script.py
+++ streamlit_script.py
@@ -1,6 +1,7 @@
 # importing necessary libraries
 import streamlit as st
 from PIL import Image
+import main
 
 
 # streamlit application function
@@ -8,6 +9,13 @@ def streamlit_app():
     image = Image.open("gemini.jpg")
     st.image(image, caption="Gemini", use_container_width=True)
     st.title("Gemini Chatbot")
+
+    left_column, right_column = st.columns(2)
+
+    left_column.write("How Can I Help You?")
+    user_input = left_column.text_input(
+        "User Input", placeholder="Type your message here..."
+    )
 
 
 if __name__ == "__main__":
