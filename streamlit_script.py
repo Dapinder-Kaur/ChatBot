a1cb421c9ec53874fbe1b2c683200b8780b59af7
Dapinder Kaur <172073817+Dapinder-Kaur@users.noreply.github.com>
2025-05-08 14:09:48 -0500

    fully working scripts including main.py and streamlit_script.py

--- streamlit_script.py
+++ streamlit_script.py
@@ -11,18 +11,21 @@ st.title("Your personal Chatbot")
 
 left_column, right_column = st.columns(2)
 
-input = input_text()
-left_column.text_input("How Can I help: ", input, key="input_text")
-st.button("Submit", key="submit_button")
-
-output_print = chatbot_stream_response(input)
+user_input = input_text()
+left_column.text_input("How Can I help: ", user_input, key="input_text")
 
 
-# st.session_state.input_text
 with right_column:
-    output = chatbot_response(input)
+    output = chatbot_response(user_input)
     output_final = output_function(output)
     st.write("Gemini")
     st.write(output_final)
+
+
+output_print = chatbot_stream_response(user_input)
+
+
+# st.session_state.input_text
+
 
 chat_output(output_print)
