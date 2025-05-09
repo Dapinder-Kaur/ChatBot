78e9750ce3ff022d9f0ac29ccef88f6610af1716
Dapinder Kaur <172073817+Dapinder-Kaur@users.noreply.github.com>
2025-05-09 16:57:45 -0500

    added gradio

---
+++ gradio_app.py
@@ -0,0 +1,23 @@
+import gradio as gr
+import random
+import time
+
+
+with gr.Blocks() as demo:
+    chatbot = gr.Chatbot(type="messages")
+    msg = gr.Textbox()
+    clear = gr.ClearButton([msg, chatbot])
+
+    def respond(message, chat_history):
+        bot_message = random.choice(
+            ["How can I help you?", "What do you need?", "I am here to assist you."]
+        )
+        chat_history.append({"role": "user", "content": message})
+        chat_history.append({"role": "assistant", "content": bot_message})
+        time.sleep(2)
+
+        return "", chat_history
+
+    msg.submit(respond, [msg, chatbot], [msg, chatbot], queue=False)
+
+demo.launch()
